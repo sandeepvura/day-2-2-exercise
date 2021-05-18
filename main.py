2d7c97aaa33cb583f975911c616f1ad01e6cbd84
sandeepvura <>
2021-05-18 18:01:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+# print(type(height))
+# print(type(weight))
+
+height_con = float(height)
+#print(type(height_con))
+
+
+weight_con = int(weight)
+#print(type(weight_con))
+# results = weight / height ** height
+# print(int((results)))
+
+results = print(int(weight_con / (height_con * height_con)))
+
+
+
+
+
+
+
+
